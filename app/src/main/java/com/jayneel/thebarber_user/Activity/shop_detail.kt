c3fc0c2cb36e3b91927e0435199d785c2146481d
Jayneelkumar Patel <56185597+jayneelpatel1@users.noreply.github.com>
2020-05-24 16:23:48 +0530

    validation on confirm book button

--- app/src/main/java/com/jayneel/thebarber_user/Activity/shop_detail.kt
+++ app/src/main/java/com/jayneel/thebarber_user/Activity/shop_detail.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.core.view.get
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.chip.Chip
 import com.google.firebase.database.DataSnapshot
@@ -13,6 +16,7 @@ import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.jayneel.thebarber_user.R
 import com.jayneel.thebarber_user.helper.shopIteamAdapter
+import com.jayneel.thebarber_user.module.appinmrntMoule
 import com.jayneel.thebarber_user.module.iteamModule
 import kotlinx.android.synthetic.main.activity_shop_detail.*
 import java.text.SimpleDateFormat
@@ -79,19 +83,206 @@ class shop_detail : AppCompatActivity() {
 
 
 
+
         chipGroup.check(R.id.chip3)
         var chiptext=getchiptext()
         var st:String?= "null"
 
 
 
+
+        val ref= database.getReference("appinment")
+        var arlist=ArrayList<String>()
+        ref.child(sunm.toString()).child(chiptext).child("time").addValueEventListener(object :ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                    for (v in p0.children)
+                    {
+                        var value=v.getValue(String::class.java)
+                        arlist.add(value.toString())
+                    }
+                for (i in arlist)
+                {
+                    if(chip21.text.equals(i))
+                        chip21.visibility=View.GONE
+                    if(chip20.text.equals(i))
+                        chip20.visibility=View.GONE
+                    if(chip2.text.equals(i))
+                        chip2.visibility=View.GONE
+                    if(chip5.text.equals(i))
+                        chip5.visibility=View.GONE
+                    if(chip6.text.equals(i))
+                        chip6.visibility=View.GONE
+                    if(chip7.text.equals(i))
+                        chip7.visibility=View.GONE
+                    if(chip8.text.equals(i))
+                        chip8.visibility=View.GONE
+                    if(chip9.text.equals(i))
+                        chip9.visibility=View.GONE
+                    if(chip10.text.equals(i))
+                        chip10.visibility=View.GONE
+                    if(chip11.text.equals(i))
+                        chip11.visibility=View.GONE
+                    if(chip12.text.equals(i))
+                        chip12.visibility=View.GONE
+                    if(chip13.text.equals(i))
+                        chip13.visibility=View.GONE
+                    if(chip14.text.equals(i))
+                        chip14.visibility=View.GONE
+                    if(chip15.text.equals(i))
+                        chip15.visibility=View.GONE
+                    if(chip16.text.equals(i))
+                        chip16.visibility=View.GONE
+                    if(chip17.text.equals(i))
+                        chip17.visibility=View.GONE
+                    if(chip18.text.equals(i))
+                        chip18.visibility=View.GONE
+                    if(chip19.text.equals(i))
+                        chip19.visibility=View.GONE
+                }
+                arlist.clear()
+              //  Toast.makeText(this@shop_detail,arlist.toString(),Toast.LENGTH_LONG).show()
+            }
+        })
+
+
+        chip3.setOnClickListener {
+            val ref= database.getReference("appinment")
+            var arlist=ArrayList<String>()
+            ref.child(sunm.toString()).child(chiptext).child("time").addValueEventListener(object :ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    for (v in p0.children)
+                    {
+                        var value=v.getValue(String::class.java)
+                        arlist.add(value.toString())
+                    }
+                    for (i in arlist)
+                    {
+                        if(chip21.text.equals(i))
+                            chip21.visibility=View.GONE
+                        if(chip20.text.equals(i))
+                            chip20.visibility=View.GONE
+                        if(chip2.text.equals(i))
+                            chip2.visibility=View.GONE
+                        if(chip5.text.equals(i))
+                            chip5.visibility=View.GONE
+                        if(chip6.text.equals(i))
+                            chip6.visibility=View.GONE
+                        if(chip7.text.equals(i))
+                            chip7.visibility=View.GONE
+                        if(chip8.text.equals(i))
+                            chip8.visibility=View.GONE
+                        if(chip9.text.equals(i))
+                            chip9.visibility=View.GONE
+                        if(chip10.text.equals(i))
+                            chip10.visibility=View.GONE
+                        if(chip11.text.equals(i))
+                            chip11.visibility=View.GONE
+                        if(chip12.text.equals(i))
+                            chip12.visibility=View.GONE
+                        if(chip13.text.equals(i))
+                            chip13.visibility=View.GONE
+                        if(chip14.text.equals(i))
+                            chip14.visibility=View.GONE
+                        if(chip15.text.equals(i))
+                            chip15.visibility=View.GONE
+                        if(chip16.text.equals(i))
+                            chip16.visibility=View.GONE
+                        if(chip17.text.equals(i))
+                            chip17.visibility=View.GONE
+                        if(chip18.text.equals(i))
+                            chip18.visibility=View.GONE
+                        if(chip19.text.equals(i))
+                            chip19.visibility=View.GONE
+                    }
+                    arlist.clear()
+                    //  Toast.makeText(this@shop_detail,arlist.toString(),Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+
+        chip4.setOnClickListener {
+            val ref= database.getReference("appinment")
+            var sr=ArrayList<String>()
+            ref.child(sunm.toString()).child(chiptext).child("time").addValueEventListener(object :ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    for (v in p0.children)
+                    {
+                        var value=v.getValue(String::class.java)
+                        sr.add(value.toString())
+                    }
+                    for (i in sr)
+                    {
+                        if(chip21.text.equals(i))
+                            chip21.visibility=View.GONE
+                        if(chip20.text.equals(i))
+                            chip20.visibility=View.GONE
+                        if(chip2.text.equals(i))
+                            chip2.visibility=View.GONE
+                        if(chip5.text.equals(i))
+                            chip5.visibility=View.GONE
+                        if(chip6.text.equals(i))
+                            chip6.visibility=View.GONE
+                        if(chip7.text.equals(i))
+                            chip7.visibility=View.GONE
+                        if(chip8.text.equals(i))
+                            chip8.visibility=View.GONE
+                        if(chip9.text.equals(i))
+                            chip9.visibility=View.GONE
+                        if(chip10.text.equals(i))
+                            chip10.visibility=View.GONE
+                        if(chip11.text.equals(i))
+                            chip11.visibility=View.GONE
+                        if(chip12.text.equals(i))
+                            chip12.visibility=View.GONE
+                        if(chip13.text.equals(i))
+                            chip13.visibility=View.GONE
+                        if(chip14.text.equals(i))
+                            chip14.visibility=View.GONE
+                        if(chip15.text.equals(i))
+                            chip15.visibility=View.GONE
+                        if(chip16.text.equals(i))
+                            chip16.visibility=View.GONE
+                        if(chip17.text.equals(i))
+                            chip17.visibility=View.GONE
+                        if(chip18.text.equals(i))
+                            chip18.visibility=View.GONE
+                        if(chip19.text.equals(i))
+                            chip19.visibility=View.GONE
+                    }
+                    sr.clear()
+                    //  Toast.makeText(this@shop_detail,arlist.toString(),Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+
+
+
+
+
+
         fb_book.setOnClickListener {
 
-            if(chipGroupSlot.isSelected && !selected.equals(null))
+           var chip= chipGroupSlot.checkedChipId
+
+            if(chip >0 &&!selected.equals(null))
             {
-                var time=findViewById<Chip>(chipGroupSlot.checkedChipId).text
+                var chipid=findViewById<Chip>(chipGroupSlot.checkedChipId)
+                var time=chipid.text
+
                 var chiptext=getchiptext()
-                bookappiment(sunm,chiptext,time.toString(),selected,unm.toString())
+                bookappiment(sunm,chiptext,time.toString(),selected,unm.toString(),chipid)
                 Toast.makeText(this,"Appinment Booked",Toast.LENGTH_LONG).show()
             }
             else
@@ -108,57 +299,62 @@ class shop_detail : AppCompatActivity() {
         date: String,
         time: String,
         selected: ArrayList<iteamModule>,
-        unm: String?
+        unm: String?,
+        chipid:Chip?
     ) {
         val myRef = database.getReference("appinment")
-        myRef.child(sunm.toString()).child(date).child(time).child(unm.toString()).setValue(selected)
-    }
-
-    private fun isappinment(
-        chiptext: String,
-        sunm: String?,
-        selected: ArrayList<iteamModule>,
-        unm: String?
-    ){
-        val myRef = database.getReference("appinment")
-        var ans= arrayListOf<iteamModule>()
-        var k=""
-        var tt:String=""
-        if (sunm != null) {
-            myRef.child(sunm).child(chiptext).addListenerForSingleValueEvent(object :ValueEventListener{
-                override fun onCancelled(p0: DatabaseError) {
-                    Toast.makeText(this@shop_detail,"Somerthing Wrog Try ofter sometime",Toast.LENGTH_LONG).show()
-                }
-
-                override fun onDataChange(p0: DataSnapshot) {
-
-                    for(v in p0.children)
-                    {
-                        k=v.key.toString()
-                    }
-                    for(v in p0.children){}
-
-                    var sum:Int=0
-                    for(t in ans)
-                    {
-                        sum=sum+t.minute.toString().toInt()
-                    }
-
-                    val formatter = DateTimeFormatter.ofPattern("HH:mm")
-                    val formatted = k.format(formatter)
-
-                        val df = SimpleDateFormat("HH:mm")
-                        val d: Date = df.parse(formatted.toString())
-                        val cal = Calendar.getInstance()
-                        cal.time = d
-                        cal.add(Calendar.MINUTE, sum)
-                        val newTime: String = df.format(cal.time)
-                    bookappiment(sunm,chiptext,newTime.toString(),selected,unm)
-                }
-            })
+        var ap=appinmrntMoule(date,time,selected,unm)
+        myRef.child(sunm.toString()).child(date).child(time).setValue(ap).addOnCompleteListener {
+            var key=myRef.child(sunm.toString()).child(date).child("time").push().key
+            myRef.child(sunm.toString()).child(date).child("time").child(key.toString()).setValue(time)
         }
-
     }
+
+//    private fun isappinment(
+//        chiptext: String,
+//        sunm: String?,
+//        selected: ArrayList<iteamModule>,
+//        unm: String?
+//    ){
+//        val myRef = database.getReference("appinment")
+//        var ans= arrayListOf<iteamModule>()
+//        var k=""
+//        var tt:String=""
+//        if (sunm != null) {
+//            myRef.child(sunm).child(chiptext).addListenerForSingleValueEvent(object :ValueEventListener{
+//                override fun onCancelled(p0: DatabaseError) {
+//                    Toast.makeText(this@shop_detail,"Somerthing Wrog Try ofter sometime",Toast.LENGTH_LONG).show()
+//                }
+//
+//                override fun onDataChange(p0: DataSnapshot) {
+//
+//                    for(v in p0.children)
+//                    {
+//                        k=v.key.toString()
+//                    }
+//                    for(v in p0.children){}
+//
+//                    var sum:Int=0
+//                    for(t in ans)
+//                    {
+//                        sum=sum+t.minute.toString().toInt()
+//                    }
+//
+//                    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+//                    val formatted = k.format(formatter)
+//
+//                        val df = SimpleDateFormat("HH:mm")
+//                        val d: Date = df.parse(formatted.toString())
+//                        val cal = Calendar.getInstance()
+//                        cal.time = d
+//                        cal.add(Calendar.MINUTE, sum)
+//                        val newTime: String = df.format(cal.time)
+//                    bookappiment(sunm,chiptext,newTime.toString(),selected,unm)
+//                }
+//            })
+//        }
+//
+//    }
     private fun getchiptext():String {
         return findViewById<Chip>(chipGroup.checkedChipId).text.toString()
     }
---
+++ app/src/main/java/com/jayneel/thebarber_user/module/appinmrntMoule.kt
@@ -0,0 +1,3 @@
+package com.jayneel.thebarber_user.module
+
+data class appinmrntMoule(var date:String?="",var time:String?="",var bookfor:ArrayList<iteamModule>,var custunm:String?="")
